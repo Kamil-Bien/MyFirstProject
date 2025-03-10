1735de52ea7ee037fdead66f6dac97138b2d96d3
Kamil-Bien <1kamilbien1@gmail.com>
2025-03-10 13:54:43 +0100

    Dodawanie pkt w pamięci działa poprawnie

---
+++ MyFirstProject/MyFirstProject/BoardGame.cs
@@ -0,0 +1,49 @@
+﻿namespace MyFirstProject
+{
+    public class BoardGame : BoardGameBase
+    {
+       // public override event GradeAddedDelegate GradeAdded;
+        public BoardGame(string name) //konstruktor
+            : base(name)
+        {
+        }
+
+        private List<float> score = new List<float>();
+
+        public override void AddReviewPoints(float numberOfPionts)
+        {
+            if (numberOfPionts >= 0 && numberOfPionts <= 10)
+            {
+                this.score.Add(numberOfPionts);
+
+                //if (GradeAdded != null)
+                //{
+                //    GradeAdded(this, new EventArgs());
+                //}
+            }
+            else
+            {
+                throw new Exception("Podana liczba musi znajdować się w zakresie 0-10");
+            }
+        }
+        //public override Statistics GetStatistics()
+        //{
+        //    var statistics = new Statistics();
+        //    foreach (var item in this.score)
+        //    {
+        //        statistics.AddScore(item);
+        //    }
+        //    if (statistics.Count > 0)
+        //    {
+        //        return statistics;
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Ten pracownik nie ma żadnych ocen");
+        //    }
+
+        //}
+
+
+    }
+}
--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,1 +1,52 @@
-﻿//główny program
+﻿using MyFirstProject;
+
+void EmployeeGradeAdded(object sender, EventArgs args)
+{
+    Console.WriteLine("Dodano nową ocenę");
+}
+
+//tworzenie nowych obiektów (pracowników) korzystając z klasy Employee
+var BoardGame1 = new BoardGame("Catan");
+//worker1.GradeAdded += EmployeeGradeAdded;
+
+Console.WriteLine("Podaj ocenę pracownika mieszczącą się w przedziale od 0 do 10");
+
+
+Console.WriteLine("(Jeżeli chcesz odrazu zakończyć wciśnij Q a następnie ENTER)");
+while (true)
+{
+    var input = Console.ReadLine();
+    if ((input == "q") || (input == "Q"))
+    {
+        break;
+    }
+
+
+    try
+    {
+        BoardGame1.AddReviewPoints(input);
+    }
+    catch (Exception toJestTymczasowaZmienna)
+    {
+        Console.WriteLine($"Wykryto wyjątek: {toJestTymczasowaZmienna.Message}");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine("Możesz podać kolejną ocenę lub zakończyć podając Q jako kolejną wartość");
+}
+//try
+//{
+//    var statistics = worker1.GetStatistics();
+//    Console.WriteLine();
+//    Console.WriteLine($"Minimalna wartość punktowa otrzymana przez tego pracownika to {statistics.Min}pkt");
+//    Console.WriteLine($"Maksymalna wartość punktowa otrzymana przez tego pracownika to {statistics.Max}pkt");
+//    Console.WriteLine($"Średnia wartość ocen tego pracownika wynosi {statistics.Average:N2}pkt");
+//    Console.WriteLine($"Ten pracownik osiągnął poziom {statistics.AverageLetter:N2}");
+//}
+//catch (Exception toJestTymczasowaZmienna)
+//{
+//    Console.WriteLine($"Wykryto wyjątek: {toJestTymczasowaZmienna.Message}");
+//}
+Console.WriteLine();
+Console.WriteLine("DZIĘKUJEMY ZA SKORZYSTANIE Z NASZEGO PROGRAMU");
